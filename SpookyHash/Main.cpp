5c3d0091e5910ee3683f855f0d7d4e3afca081fe
ericaw29 <38544284+ericaw29@users.noreply.github.com>
2020-04-18 21:07:22 -0400

    Add files via upload
    new spookyhash with 128， 64， 32

--- SpookyHash/Main.cpp
+++ SpookyHash/Main.cpp
@@ -9,28 +9,11 @@
 #include <iostream>
 #include <fstream>
 
-std::string TestResults(int seed, std::string str)
+std::string TestResults(int seed, const void * message, size_t size)
 {
     uint64 hash1 = seed;
     uint64 hash2 = seed;
-    //char shorturl[4][100];
-    const void * a;
-    //char url[4][100] = {"geeksforgeeksarraystringsc3differentwayscreate", "youtubewatchvLDGrxUBcNk", "youtubecomwatchvvzEjhC1kmY", "drivegoogledriveu1folders1poJ4O9CoE6ZZD2yerx3jLT4SqgdHctsb"};
-    //for (int i = 0; i<4; i++){
-      //  printf ("--------------------\n initial url is --> %s \n", url[i]);
-        //std::string str = url[i];
-        //size_t size = str.size();
-        //printf("the unsigned decimal of the url %zu\n", size);  // prints as unsigned decimal
-        //printf("the hex of the url %zx\n", size);  // prints as hex
-        //printf("the signed decimal of the url %zd\n", size);  // prints as signed decimal
-        //a = str.c_str();
-        //SpookyHash::Hash128(a, size, &hash1, &hash2);
-        //printf("data %d %p\n", *(const int *) a, a);
-        //printf("%" PRIu64 "\n", hash1);
-        //printf("%" PRIu64 "\n", hash2);
-    size_t size = str.size();
-    a = str.c_str();
-    SpookyHash::Hash128(a, size, &hash1, &hash2);
+    SpookyHash::Hash128(message, size, &hash1, &hash2);
     std::string hashOne = std::to_string(hash1);
     std::string hashTwo = std::to_string(hash2);
     std::string whole = hashOne + hashTwo;
@@ -43,16 +26,36 @@ std::string TestResults(int seed, std::string str)
 
 int main(int argc, const char **argv)
 {
+    
+
     std::ofstream newFile;
     newFile.open("hash128bit.csv");
     std::ifstream myFile;
     myFile.open("originalURL.csv");
     std::string line;
+    newFile << "Cleaned Original URL" << "," << "128 bit Hash Value" << "," << "64 bit Hash Value" << "," << "32 bit Hash Value" << "\n";
+
+    size_t mesgLength;
+    std::string result;
+    std::string result64;
+    std::string result32;
+    
+    
+    
+    
     while (myFile.good()){
         getline(myFile, line, '\n');
-        std::string result = TestResults(argc, line);
+        const void * mesg = line.c_str();
+        mesgLength = line.size();
+        //128 bit hash value
+        result = TestResults(argc, mesg, mesgLength);
+        //64
+        result64 = std::to_string(SpookyHash::Hash64(mesg, mesgLength, 0));
+        //32
+        result32 = std::to_string(SpookyHash::Hash32(mesg, mesgLength, 0));
+        
         line.erase(line.length()-1);
-        newFile << line << "," << result << "\n";
+        newFile << line << "," << result << "," << result64 << "," << result32 << "\n";
         //printf("%s \n", line.c_str());
         // size_t size = line.size();
         //printf("the unsigned decimal of the url %zu\n", size);
