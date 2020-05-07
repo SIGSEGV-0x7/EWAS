3db974c9fba89529e66bf43698cd145a83c1da21
Xiaolin Zhou <42752035+xiaolin98@users.noreply.github.com>
2020-05-07 19:41:57 -0400

    add input & output file for simpleclean, simplehash and xxhash

--- SimpleHash/basic_clean_url.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-import re
-
-data = pd.read_csv("origin_surt_url.csv", names=["origin", "surt"], header=None)
-data["origin_clean"] = data["origin"].apply(lambda url : re.sub('[^A-Za-z0-9]+', '', url[5:]))
-data.drop("surt", axis = 1).to_csv("origin_simpleclean.csv", header = False, index = False)
